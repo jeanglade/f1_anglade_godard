4c6c98d9aaf4763db98f2f482880dc758c96389b
Jean Anglade <62482904+jeanglade@users.noreply.github.com>
2022-01-12 08:31:41 +0100

    Initial commit

---
+++ app/src/main/java/com/example/f1_anglade_godard/ui/notifications/NotificationsViewModel.kt
@@ -0,0 +1,13 @@
+package com.example.f1_anglade_godard.ui.notifications
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class NotificationsViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is notifications Fragment"
+    }
+    val text: LiveData<String> = _text
+}
